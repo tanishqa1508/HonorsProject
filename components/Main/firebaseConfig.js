6e322ed213b305022b1811c335952df3679c8f11
tanishqa1508 <96232293+tanishqa1508@users.noreply.github.com>
2024-04-25 22:52:22 +0530

    Add files via upload

---
+++ components/Main/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWLXX95aALwME5UtVg2wNAqb3v96j5ylg",
+  authDomain: "social-app-17b20.firebaseapp.com",
+  projectId: "social-app-17b20",
+  storageBucket: "social-app-17b20.appspot.com",
+  messagingSenderId: "633837222059",
+  appId: "1:633837222059:web:8d2711c78cf73f8ea79fee",
+  measurementId: "G-VZQEDD6X2X",
+};
